73d8bfcac7c3d74fda42324d95b327cf8abe5c6d
vedantgrover <vedantvgrover@gmail.com>
2022-12-23 11:31:01 -0800

    Initializing Mod Settings

---
+++ src/main/java/com/freyr/bc/BladeCraftClient.java
@@ -0,0 +1,10 @@
+package com.freyr.bc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BladeCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
