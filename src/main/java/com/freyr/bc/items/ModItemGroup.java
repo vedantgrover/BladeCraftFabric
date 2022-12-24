e913d2cff76e76e61edc53af6564a4d75d2dbceb
vedantgrover <vedantvgrover@gmail.com>
2022-12-24 10:38:36 -0800

    Adding Sapphire and more Ruby Stuff

--- src/main/java/com/freyr/bc/items/ModItemGroup.java
+++ src/main/java/com/freyr/bc/items/ModItemGroup.java
@@ -9,4 +9,5 @@ import net.minecraft.util.Identifier;
 public class ModItemGroup {
 
     public static final ItemGroup RUBY_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(BladeCraft.MOD_ID, "ruby_item_group"), () -> new ItemStack(ModItems.RUBY));
+    public static final ItemGroup SAPPHIRE_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(BladeCraft.MOD_ID, "sapphire_item_group"), () -> new ItemStack(ModItems.SAPPHIRE));
 }
--- src/main/java/com/freyr/bc/items/ModItems.java
+++ src/main/java/com/freyr/bc/items/ModItems.java
@@ -3,13 +3,16 @@ package com.freyr.bc.items;
 import com.freyr.bc.BladeCraft;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings().group(ModItemGroup.RUBY_ITEM_GROUP)));
+    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings().group(ModItemGroup.RUBY_ITEM_GROUP)));
+
+    public static final Item SAPPHIRE = registerItem("sapphire", new Item(new FabricItemSettings().group(ModItemGroup.SAPPHIRE_ITEM_GROUP)));
+    public static final Item RAW_SAPPHIRE = registerItem("raw_sapphire", new Item(new FabricItemSettings().group(ModItemGroup.SAPPHIRE_ITEM_GROUP)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(BladeCraft.MOD_ID, name), item);
